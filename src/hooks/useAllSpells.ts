de07c9eb58f11e7e06ad28cf953c3330453dc6d4
BUGGYPC\komme <mschmidt-ext@goodgamestudios.com>
2025-05-04 23:14:20 +0200

    removed api to hide img src for now, since database calls are super high

--- src/hooks/useAllSpells.ts
+++ src/hooks/useAllSpells.ts
@@ -6,25 +6,25 @@ import useSWR from 'swr'
 
 export function useAllSpells(): Spell[] | undefined {
     const { data: spells, error } = useSWR<Spell[]>(
-      'all-spells',
-      fetchAllSpells,
-      {
-        revalidateOnFocus: false,
-        dedupingInterval: 1000 * 60 * 60, 
-      }
+        'all-spells',
+        fetchAllSpells,
+        {
+            revalidateOnFocus: false,
+            dedupingInterval: 1000 * 60 * 60,
+        }
     )
-  
+
     useEffect(() => {
-      if (!spells) return
-      spells.forEach((s) => {
-        const img = new Image()
-        img.src = `/api/image/${s.id}`
-      })
+        if (!spells) return
+        spells.forEach((s) => {
+            const img = new Image()
+            img.src = s.iconUrl
+        })
     }, [spells])
-  
+
     if (error) {
-      console.error('Failed to load spells', error)
-      return []
+        console.error('Failed to load spells', error)
+        return []
     }
     return spells
-  }
+}
